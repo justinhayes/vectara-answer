d4e5408c5bcddcf08e91c0737cde4424991a794c
justinhayes <justin.a.hayes@gmail.com>
2024-03-12 18:03:22 -0700

    Update parseSnippet.ts
    Add regex to strip HTML markup from the output

--- src/utils/parseSnippet.ts
+++ src/utils/parseSnippet.ts
@@ -2,7 +2,7 @@ export const START_TAG = "%START_SNIPPET%";
 export const END_TAG = "%END_SNIPPET%";
 
 export const parseSnippet = (source: string) => {
-  const [pre, textAndPost] = source.indexOf(START_TAG) !== -1 ? source.split(START_TAG) : ["", source];
-  const [text, post] = textAndPost.indexOf(END_TAG) !== -1 ? textAndPost.split(END_TAG) : [textAndPost, ""];
+  const [pre, textAndPost] = source.indexOf(START_TAG) !== -1 ? source.replace(/(<([^>]+)>)/gi, '').split(START_TAG) : ["", source];
+  const [text, post] = textAndPost.indexOf(END_TAG) !== -1 ? textAndPost.replace(/(<([^>]+)>)/gi, '').split(END_TAG) : [textAndPost, ""];
   return { pre, post, text };
 };
