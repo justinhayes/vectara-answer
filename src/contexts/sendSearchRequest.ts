6dc0f76e83546a07c86fdb2db40708375f2d3ac9
justin <justin.a.hayes@gmail.com>
2024-03-19 06:41:42 -0700

    adding related content link

--- src/contexts/sendSearchRequest.ts
+++ src/contexts/sendSearchRequest.ts
@@ -14,6 +14,7 @@ type Config = {
   hybridNumWords: number;
   hybridLambdaShort?: number;
   hybridLambdaLong?: number;
+  mode?: string;
   summaryNumResults?: number;
   summaryNumSentences?: number;
   summaryPromptName?: string;
@@ -36,6 +37,7 @@ export const sendSearchRequest = async ({
   hybridNumWords,
   hybridLambdaShort,
   hybridLambdaLong,
+  mode,
   summaryNumResults,
   summaryNumSentences,
   summaryPromptName,
@@ -56,7 +58,8 @@ export const sendSearchRequest = async ({
       lexicalInterpolationConfig: {
         lambda: lambda,
       },
-      metadataFilter: filter ? `doc.source = '${filter}'` : undefined,
+      metadataFilter: filter ? `part.lang = '${filter}'` : undefined,
+      "semantics": mode ? `RESPONSE` : undefined,
     };
   });
 
